c097c596d2b631dbe55b5bd382a71d40ad47ccb0
v0agent <it+v0agent@vercel.com>
2026-06-04 21:07:54 +0000

    feat: update TikTok Pixel ID in layout component\n\nUpdated TikTok Pixel ID for tracking purposes

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
         />
         {/* Utmify TikTok Pixel */}
         <Script id="utmify-tiktok" strategy="afterInteractive">{`
-          window.tikTokPixelId = "6a10b97ec68c45ed8ef59791";
+          window.tikTokPixelId = "6a21e6cc38f9773b0c05e4c5";
           var a = document.createElement("script");
           a.setAttribute("async", "");
           a.setAttribute("defer", "");
